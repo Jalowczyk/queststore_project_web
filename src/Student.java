7b4f060b93636ff3dcc7d20569836cf9b30aa6db
piotrtar <piotr.tarczon@gmail.com>
2017-08-30 13:40:38 +0200

    Create Student class

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Student extends AbstractUser {
+
+    ArrayList<Quest> quests = new ArrayList<Quest>();
+
+    public Student(String name, String surname, Character[] password, String login){
+        super(name, surname, password, login);
+    }
+
+
+}
