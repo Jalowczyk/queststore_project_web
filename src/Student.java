ccd8eb0c302dafa41d5e79321c0ee04f9f64cc13
Grzegorz Winiarek <g.d.winiarek@gmail.com>
2017-08-31 14:57:58 +0200

    added all get/set method to Student model

--- src/Student.java
+++ src/Student.java
@@ -2,11 +2,43 @@ import java.util.ArrayList;
 
 public class Student extends AbstractUser {
 
-    ArrayList<Quest> quests = new ArrayList<Quest>();
+    ArrayList<Quest> studentQuests = new ArrayList<Quest>();
 
-    public Student(String name, String surname, Character[] password, String login){
-        super(name, surname, password, login);
+    public Student(){}
+
+    public Student(String name, String surname, String login, String id){
+        super(name, surname, password, login, id);
+        this.wallet = new Wallet();
     }
-
-
+    @Override
+    public String toString() {
+      return name, surname, login, id;
+    }
+    public getStudentName(){
+      return this.name;
+    }
+    public getStudentSurname(){
+      return this.surname;
+    }
+    public getStudentLogin(){
+      return this.login;
+    }
+    public getStudentId(){
+      return this.id;
+    }
+    public getWallet(){
+      return this.wallet;
+    }
+    public void setStudentName(String name){
+      this.name = name;
+    }
+    public void setStudentSurname(String surname){
+      this.surname = surname;
+    }
+    public void setStudentPassword(String password){
+      this.password = password;
+    }
+    public void setStudentId(String id){
+      this.id = id;
+    }
 }
