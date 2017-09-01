6ade23d3a0650fcac267fb76f53e5f456661c566
piotrtar <piotr.tarczon@gmail.com>
2017-09-01 09:33:02 +0200

    Update Wallet class

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 
 public class Student extends AbstractUser {
-
+    
     ArrayList<Quest> quests = new ArrayList<Quest>();
 
     public Student(String name, String surname, Character[] password, String login){
         super(name, surname, password, login);
+        this.wallet = new Wallet();
     }
+
 
 
 }
---
+++ src/WalletInterface.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public interface WalletInterface {
+    public void updateBalance(int amout);
+    public void updateExperience(int amount);
+}
