a45e42486efc8e87f17b0bea6238c30c3d79e06b
Val1706 <potapovvalentine@gmail.com>
2017-09-01 18:48:25 +0200

    Add  admin controller to controlers package

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,14 +1,23 @@
-package src.controllers;
-
-import src.models.Admin;
 
 public class AdminController {
 
 
-    public static void startController(Admin admin) {
+    public static void startController(Admin admin){
         System.out.println(admin.getName());
     }
-
+    
     public static void createMentor(String name, String surname, Character[] password, String login, String id) {
+//        Mentor mentor = new Mentor(name, surname, password, login, id);
+//        School.addMentor(mentor);
+//    }
+//
+//    //public static Admin createAdmin(String name, String surname, Character[] password, String login, String id){
+//    //   Admin admin = new Admin(name, surname, password, login, id);
+//    //    return admin;
+//    //}
+//
+//}
+
     }
 }
+
