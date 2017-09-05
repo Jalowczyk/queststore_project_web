84436a7206ba3b6dd6cd52f0505c93d3a959e542
Val1706 <potapovvalentine@gmail.com>
2017-09-06 01:52:11 +0200

    connected sql instead of csv files, implemented sql methods in Abstract Dao Class

--- src/dao/DAOAdmin.java
+++ src/dao/DAOAdmin.java
@@ -2,15 +2,13 @@ package src.dao;
 
 import src.models.Admin;
 
-import java.io.*;
-
 public class DAOAdmin extends AbstractDAO {
 
-    private static final File f = new File("src/csv/Admins.csv");
-    private static final String path = f.getAbsolutePath();
+    private static final String dataBasePath = "jdbc:sqlite:src/sql/admins.db";
+    private static final String dataBaseName = "INSERT INTO `admins`(name,surname,password,login,mail)";
     private static Admin admin = new Admin();
 
     public DAOAdmin() {
-        super(admin, path);
+        super(dataBaseName, dataBasePath, admin);
     }
 }
