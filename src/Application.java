995dc6c55481531c8b1f275016de91a1e3e51d22
piotrtar <piotr.tarczon@gmail.com>
2017-09-06 17:57:06 +0200

    Refactory Application module

--- src/Application.java
+++ src/Application.java
@@ -2,6 +2,7 @@ package src;
 
 import src.controllers.AdminController;
 import src.controllers.MentorController;
+import src.controllers.LoginController;
 import src.models.User;
 import src.views.ApplicationView;
 import src.dao.DAOAdmin;
@@ -10,72 +11,6 @@ import src.dao.DAOMentor;
 public class Application {
     
     public static void main(String[] args) {
-        mainController();
-    }
-
-    public static void mainController() {
-
-        ApplicationView.setUserLoginMsg();
-        String choice = ApplicationView.getUserChoiceInput();
-
-        ApplicationView.setUserId();
-        String id = ApplicationView.getInput();
-
-        startLoginProcess(choice, id);
-    }
-
-    public static void startLoginProcess(String choice, String id) {
-
-        if (choice.equals("1")) {
-            adminLoginProcess(id);
-
-        } else if (choice.equals("2")) {
-            mentorLoginProcess(id);
-        }
-    }
-
-    public static User getAdmin(String id) {
-        DAOAdmin admin = new DAOAdmin();
-        User createdAdmin = admin.load(id);
-        return createdAdmin;
-    }
-
-    public static User getMentor(String id) {
-        DAOMentor mentor = new DAOMentor();
-        User createdMentor = mentor.load(id);
-        return createdMentor;
-    }
-
-    public static void adminLoginProcess(String id) {
-
-        User user = getAdmin(id);
-
-        if (user != null) {
-
-            AdminController.startController(user);
-        } else {
-            ApplicationView.failedLoginMsg();
-        }
-    }
-        
-    public static void mentorLoginProcess(String id) {
-
-        User user = getMentor(id);
-
-        if (user != null) {
-
-            MentorController.startController(user);
-
-        } else {
-            ApplicationView.failedLoginMsg();
-        }
+        LoginController.roleSetting();
     }
 }
-
-
-
-
-
-
-
-
