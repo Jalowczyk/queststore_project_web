910b9c29bf234558ce28135ffd9c8408fc46ab2e
piotrtar <piotr.tarczon@gmail.com>
2017-09-07 16:11:43 +0200

    Update Application module

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,6 @@
 package src;
 
-import src.controllers.AdminController;
-import src.controllers.MentorController;
 import src.controllers.LoginController;
-import src.models.User;
-import src.views.ApplicationView;
-import src.dao.DAOAdmin;
-import src.dao.DAOMentor;
 
 public class Application {
     
