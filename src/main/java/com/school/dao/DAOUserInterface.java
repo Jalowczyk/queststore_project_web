7657aac28173c0a7c3db11540baf21061597fec4
Val1706 <potapovvalentine@gmail.com>
2017-09-21 15:44:11 +0200

    changed mrthods in interface

--- src/main/java/com/school/dao/DAOUserInterface.java
+++ src/main/java/com/school/dao/DAOUserInterface.java
@@ -4,8 +4,9 @@ import com.school.models.User;
 
 import java.io.IOException;
 
-public interface DAOInterface {
+public interface DAOUserInterface {
 
     User load(String id);
+    void save(User user);
     User createFromRow(Integer id, String... values);
 }
