7b8ad8201ad1e799e1ac861009d5b5d62c7a5762
Val1706 <potapovvalentine@gmail.com>
2017-09-21 16:45:33 +0200

    Update DaoMentor.java

--- src/main/java/com/school/dao/DaoMentor.java
+++ src/main/java/com/school/dao/DaoMentor.java
@@ -10,6 +10,18 @@ public class DaoMentor extends UserDao {
     private User mentor;
 
     public DaoMentor(User mentor) { this.mentor = mentor; }
+    
+    public static void editMentor(String first_name, Integer id) {
+
+        String query = "UPDATE users SET first_name = '" + first_name + "'" +
+                       "WHERE id_number = '" + id + "'";
+        try {
+            Statement st = conn.createStatement();
+            st.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void save() {
 
