39c9d1f934f63fdbeba650457791e83135068c7a
Val1706 <potapovvalentine@gmail.com>
2017-09-22 09:07:38 +0200

    Add files via upload

---
+++ src/main/java/com/school/dao/DaoCourse.java
@@ -0,0 +1,11 @@
+package com.school.dao;
+
+import java.sql.SQLException;
+
+public class DaoCourse extends AbstractDao{
+
+    public DaoCourse(String tableName){
+        super(tableName);
+    }
+
+}
