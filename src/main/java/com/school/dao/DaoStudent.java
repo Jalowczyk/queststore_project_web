e7724f6fb36c97fb2f4165106299932a7b801ffd
Val1706 <potapovvalentine@gmail.com>
2017-09-26 01:04:06 +0200

    Add method to save student id to table students

--- src/main/java/com/school/dao/DaoStudent.java
+++ src/main/java/com/school/dao/DaoStudent.java
@@ -1,14 +1,41 @@
 package com.school.dao;
+import com.school.models.Student;
 import com.school.models.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DaoStudent extends UserDao {
 
-    private User student;
+    private Student student;
 
-    public DaoStudent (User student) {this.student = student;}
+    public DaoStudent(Student student) {
+        this.student = student;
+    }
 
 
-    public void save(){
+    public void save() {
+
         save(student);
+        saveCourse();
+    }
+
+    public void saveCourse() {
+
+        String query = "INSERT INTO students (student_id, course_id) VALUES(?,?)";
+
+        try(PreparedStatement statement = conn.prepareStatement(query)){
+
+            statement.setInt(1, UserDao.getLastId());
+            statement.setInt(2, student.getCourse().getId());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+
