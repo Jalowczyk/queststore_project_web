f3f6926fae833aa5af2974d75973b5bd97ea99dd
Val1706 <potapovvalentine@gmail.com>
2017-10-03 21:43:13 +0200

    Add inputs for providing course info

--- src/main/java/com/school/views/AdminView.java
+++ src/main/java/com/school/views/AdminView.java
@@ -1,31 +1,35 @@
 package com.school.views;
 
+import com.school.models.Course;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AdminView extends AbstractView{
+public class AdminView extends AbstractView {
 
     private static Scanner scan = new Scanner(System.in);
-    private static int inputNum; 
-    
+    private static int inputNum;
+    private static Scanner scanForNumbers = new Scanner(System.in);
+
     public static String getChoice() {
-        Scanner scanForNumbers = new Scanner(System.in);
+
         do {
             System.out.println("Please choose an option: ");
             inputNum = scanForNumbers.nextInt();
-        } while (inputNum <0 | inputNum >6);
+        } while (inputNum < 0 | inputNum > 6);
 
         return Integer.toString(inputNum);
     }
 
     public static void showMenu() {
         String menu = "\n1. Create mentor"
-                        + "\n2. Create new Course"
-                        + "\n3. Edit chosen Mentor profile"
-                        + "\n4. Show info about chosen Course"
-                        + "\n5. Show info about chosen Mentor"
-                        + "\n6. Set level of Experience"
-                        + "\n0. Exit";
-                        
+                + "\n2. Create new Course"
+                + "\n3. Edit chosen Mentor profile"
+                + "\n4. Show info about chosen Course"
+                + "\n5. Show info about chosen Mentor"
+                + "\n6. Set level of Experience"
+                + "\n0. Exit";
+
         System.out.println(menu);
     }
 
@@ -36,7 +40,7 @@ public class AdminView extends AbstractView{
         return name;
     }
 
-    public static String typeMentorSurname(){
+    public static String typeMentorSurname() {
 
         System.out.println("Please provide the surname of mentor: ");
         String surname = scan.nextLine();
@@ -57,13 +61,43 @@ public class AdminView extends AbstractView{
         return id;
     }
 
-    public static String typeMentorEmail(){
+    public static String typeMentorEmail() {
 
         System.out.println("Please provide the mail of mentor: ");
         String email = scan.nextLine();
         return email;
     }
 
+    public static String typeCourseName() {
+        System.out.println("Please provide name for course: ");
+        String name = scan.nextLine();
+        return name;
+    }
+
+    public static Integer typeCourseId() {
 
 
-}   
+        System.out.println("Please provide id of course: ");
+        Integer id = scanForNumbers.nextInt();
+        return id;
+    }
+
+    public static void showAllCourses(ArrayList<Course> allCourses) {
+
+        for (Course course : allCourses) {
+            System.out.println(course.getName());
+            System.out.println(course.getId());
+
+        }
+
+
+    }
+
+    public static void showCourseInfo(Course course) {
+        System.out.println(course.getName());
+
+    }
+
+}
+
+
