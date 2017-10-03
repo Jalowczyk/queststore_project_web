76f3fea5e2b378219c5040d9d1a8a37105233dbb
Val1706 <potapovvalentine@gmail.com>
2017-10-03 15:19:58 +0200

    Update methods

--- src/main/java/com/school/views/MentorView.java
+++ src/main/java/com/school/views/MentorView.java
@@ -1,34 +1,37 @@
 package com.school.views;
 
+import com.school.models.User;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MentorView extends AbstractView{
+public class MentorView extends AbstractView {
 
     private static Scanner scan = new Scanner(System.in);
-    private static int inputNum; 
-    
+    private static int inputNum;
+
     public static String getChoice() {
         Scanner scanForNumbers = new Scanner(System.in);
         do {
             System.out.println("Please choose an option: ");
             inputNum = scanForNumbers.nextInt();
-        } while (inputNum <0 | inputNum >8);
+        } while (inputNum < 0 | inputNum > 8);
 
         return Integer.toString(inputNum);
     }
 
     public static void showMenu() {
         String menu = "\n1. Create student"
-                        + "\n2. Show all courses"
-                        + "\n3. Manage Quests"
-                        + "\n4. Manage Artefacts"
-                        + "\n5. Show info about all Students"
-                        + "\n6. Mark Student's Artefacts"
-                        + "\n7. Mark Student's Quests"
-                        + "\n8. Set level experience"
-                        + "\n0. Exit";
-                        
-        System.out.println(menu);    
+                + "\n2. Show all courses"
+                + "\n3. Manage Quests"
+                + "\n4. Manage Artefacts"
+                + "\n5. Show info about all Students"
+                + "\n6. Mark Student's Artefacts"
+                + "\n7. Mark Student's Quests"
+                + "\n8. Set level experience"
+                + "\n0. Exit";
+
+        System.out.println(menu);
     }
 
     public static String typeStudentName() {
@@ -38,7 +41,7 @@ public class MentorView extends AbstractView{
         return name;
     }
 
-    public static String typeStudentSurname(){
+    public static String typeStudentSurname() {
 
         System.out.println("Please provide the student's surname: ");
         String surname = scan.nextLine();
@@ -52,25 +55,65 @@ public class MentorView extends AbstractView{
         return password;
     }
 
-    public static String typeStudentMail(){
+    public static String typeStudentMail() {
         System.out.println("Please provide the student's mail: ");
         String mail = scan.nextLine();
         return mail;
     }
 
-    public static Integer getMentorId(){
+    public static Integer getMentorId() {
         System.out.println("Please provide mentor's id: ");
         Integer id = scan.nextInt();
         return id;
     }
 
-    public static Integer getCourseId(){
+    public static Integer getCourseId() {
         System.out.println("Please provide course id: ");
         Integer id = scan.nextInt();
         return id;
     }
 
+    public static void showStudent(User mentor) {
+
+        System.out.println(mentor.getName());
+        System.out.println(mentor.getSurname());
+        System.out.println(mentor.getId());
+
+    }
+
+    public static String getQuestTitle() {
+
+        System.out.println("Please provide title for quest: ");
+        String title = scan.nextLine();
+        return title;
+    }
+
+    public static String getQuestInfo() {
+
+        System.out.println("Please provide info for quest: ");
+        String info = scan.nextLine();
+        return info;
+    }
+
+    public static Integer getQuestPrize() {
+
+        System.out.println("Please provide prize for quest: ");
+        Scanner scanForNumbers = new Scanner(System.in);
+        Integer prize = scanForNumbers.nextInt();
+
+        return prize;
+    }
+
+    public static String getQuestCategory(){
+
+        System.out.println("Please provide category for quest: ");
+        String category = scan.nextLine();
+
+        return category;
+    }
+}
 
 
 
-}   
+
+
