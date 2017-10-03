8428fb50340fc22ea44641ee7d24a26b2d22658a
Val1706 <potapovvalentine@gmail.com>
2017-10-03 15:11:39 +0200

    Create saveStudentRecords method in StudentDAO

--- src/main/java/com/school/dao/StudentDAO.java
+++ src/main/java/com/school/dao/StudentDAO.java
@@ -1,8 +1,12 @@
 package com.school.dao;
 import com.school.models.Student;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 
 public class StudentDAO extends UserDAO {
 
@@ -12,21 +16,22 @@ public class StudentDAO extends UserDAO {
         this.student = student;
     }
 
-
     public void save() {
-
         saveUser(student);
-        saveCourse();
+        saveStudentRecords();
     }
 
-    public void saveCourse() {
+    public void saveStudentRecords() {
 
-        String query = "INSERT INTO students (student_id, course_id) VALUES(?,?)";
+        String query = "INSERT INTO students (student_id, course_id, wallet_id) VALUES(?,?,?)";
 
-        try(PreparedStatement statement = conn.prepareStatement(query)){
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
 
-            statement.setInt(1, UserDAO.getLastCreatedId());
+
+            // System.out.println()
+            statement.setInt(1, UserDAO.getLastUserCreatedId());
             statement.setInt(2, student.getCourse().getId());
+            statement.setInt(3, WalletDAO.getLastWalletCreatedId());
 
             statement.executeUpdate();
 
@@ -34,6 +39,44 @@ public class StudentDAO extends UserDAO {
             e.printStackTrace();
         }
     }
+
+    public Integer getStudentWalletId() {
+
+        Integer studentWalletId = null;
+        String query = "SELECT wallet_id FROM students WHERE student_id = " + student.getId() + ";";
+
+        try (Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
+
+            while (rs.next()) {
+
+                studentWalletId = rs.getInt("wallet_id");
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return studentWalletId;
     }
+
+    public Integer getStudentCourseId() {
+
+        Integer studentCourseId = null;
+        String query = "SELECT course_id FROM students WHERE student_id = " + student.getId() + ";";
+
+        try (Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
+
+            while (rs.next()) {
+
+                studentCourseId = rs.getInt("course_id");
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return studentCourseId;
+    }
+}
 
 
