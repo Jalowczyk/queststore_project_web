c84521ce1e8d58760ccf9f2496966f870af2751a
Val1706 <potapovvalentine@gmail.com>
2017-10-03 15:13:54 +0200

    Create hasStudentRole method in Admin model

--- src/main/java/com/school/models/Admin.java
+++ src/main/java/com/school/models/Admin.java
@@ -10,6 +10,10 @@ public class Admin extends User {
     public Admin(String name, String surname, String password, String mail, String status) {
         super(name, surname, password, mail, status);
     }
+
+    @Override
+    boolean hasStudentRole() {return false;}
+
     @Override
     public void startController(){
         AdminController.startController(this);
