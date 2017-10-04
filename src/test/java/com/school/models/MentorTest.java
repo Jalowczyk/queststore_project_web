c9708cba6b3612ec653fc59e47eea5c1e91018de
monzag <zaganiacz.m@gmail.com>
2017-10-04 19:11:00 +0200

    Add testGetName

--- src/test/java/com/school/models/MentorTest.java
+++ src/test/java/com/school/models/MentorTest.java
@@ -1,6 +1,9 @@
 package com.school.models;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MentorTest {
 
@@ -9,6 +12,11 @@ class MentorTest {
     @BeforeEach
     public void before() {
         this.mentor = new Mentor("Mateusz", "Ostafil", "password", "mat@gmail.com", "mentor");
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Mateusz", mentor.getName());
     }
 
 }
