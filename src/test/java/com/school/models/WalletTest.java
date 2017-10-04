a815efe0091b9f96d2c0388441bf77f04926b637
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-10-04 20:33:19 +0200

    Add init of Wallet object before each test method

--- src/test/java/com/school/models/WalletTest.java
+++ src/test/java/com/school/models/WalletTest.java
@@ -1,7 +1,23 @@
 package com.school.models;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class WalletTest {
 
+    private Wallet wallet;
+
+    @BeforeEach
+    public void before() {
+        ArrayList<Artifact> artifacts = new ArrayList<Artifact>();
+        artifacts.add(new Artifact("artifact1"));
+        artifacts.add(new Artifact("artifact2"));
+
+        this.wallet = new Wallet(1, 100, 100, artifacts);
+    }
+
+    
 }
