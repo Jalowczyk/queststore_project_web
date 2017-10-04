f57ac3fc1fa71938ef741dc4fbba9d5a5d69207d
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-10-04 20:03:41 +0200

    Add Artifact class basic template for test puropses

---
+++ src/main/java/com/school/models/Artifact.java
@@ -0,0 +1,25 @@
+package com.school.models;
+
+
+public class Artifact {
+
+    private String name;
+
+    public Artifact(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+
+}
