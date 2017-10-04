049925fe6c729dcac6bd4e73389f174a55a7c274
piotrtar <piotr.tarczon@gmail.com>
2017-10-04 11:02:12 +0200

    Fix constructor in Student model

--- src/main/java/com/school/models/Student.java
+++ src/main/java/com/school/models/Student.java
@@ -12,7 +12,7 @@ public class Student extends User {
     public Student(){
     }
 
-    public Student(String name, String surname, String mail, String password, String status){
+    public Student(String name, String surname, String password, String mail, String status){
         super(name, surname, password, mail, status);
 
 
