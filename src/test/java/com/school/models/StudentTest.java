d445c68cf705743ccf9b6c3dba0a56d080d53eb3
monzag <zaganiacz.m@gmail.com>
2017-10-05 10:47:28 +0200

    Add testGetWallet

--- src/test/java/com/school/models/StudentTest.java
+++ src/test/java/com/school/models/StudentTest.java
@@ -1,6 +1,7 @@
 package com.school.models;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +12,11 @@ class StudentTest {
     @BeforeEach
     public void before() {
         this.student = new Student("Adam", "Kowalski", "adam@gmail.com", "xxx", "student");
+    }
+
+    @Test
+    public void testGetWallet() {
+        assertNotNull(student.getWallet());
     }
 
 }
