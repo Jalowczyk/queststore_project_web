3a0c1911d098a3068fd7431e510d3e44407bf9ca
monzag <zaganiacz.m@gmail.com>
2017-10-05 09:59:49 +0200

    Remove testStartController

--- src/test/java/com/school/models/MentorTest.java
+++ src/test/java/com/school/models/MentorTest.java
@@ -1,11 +1,10 @@
 package com.school.models;
 
+import com.school.controllers.MentorController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MentorTest {
 
