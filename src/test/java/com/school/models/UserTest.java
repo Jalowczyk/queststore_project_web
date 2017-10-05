aef557bdc24270ab411bd76bf13974d4f9cd0f56
wutismyname <plebejoosh@gmail.com>
2017-10-05 11:35:58 +0200

    Add testGetStatus()

--- src/test/java/com/school/models/UserTest.java
+++ src/test/java/com/school/models/UserTest.java
@@ -23,54 +23,8 @@ class UserTest {
 
     @Test
     void testGetStatus() {
+        assertEquals("status", this.user.getStatus());
     }
 
-    @Test
-    void testSetStatus() {
-    }
-
-    @Test
-    void testToString() {
-    }
-
-    @Test
-    void testSetName() {
-    }
-
-    @Test
-    void testGetName() {
-    }
-
-    @Test
-    void testGetSurname() {
-    }
-
-    @Test
-    void testGetId() {
-    }
-
-    @Test
-    void testSetSurname() {
-    }
-
-    @Test
-    void testSetPassword() {
-    }
-
-    @Test
-    void testGetPassword() {
-    }
-
-    @Test
-    void testSetId() {
-    }
-
-    @Test
-    void testSetMail() {
-    }
-
-    @Test
-    void testGetMail() {
-    }
 
 }
